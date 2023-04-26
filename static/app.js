d2ed79f12f790989568140415070ce987817d49a
Kamer DINC <kamerdinc@gmail.com>
2023-04-26 18:22:55 +0300

    Update app.js
    Compability for Vue3

--- static/app.js
+++ static/app.js
@@ -1,28 +1,35 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    comments: [],
-    name: '',
-    text: '',
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      comments: [],
+      name: "",
+      text: "",
+    };
   },
-  created() { this.update() },
   methods: {
-    add: () => {
-      const payload = {'name': app.name, 'text': app.text}
-      axios.post('/api/comments', payload)
+    add: function () {
+      const payload = { name: this.name, text: this.text };
+      axios
+        .post("/api/comments", payload)
         .then(() => {
-          app.name = ''
-          app.text = ''
-          app.update()
+          this.name = "";
+          this.text = "";
+          this.update();
         })
         .catch((err) => {
-          alert(err.response.data)
-        })
+          alert(err.response.data.message);
+        });
     },
-    update: () => {
-      axios.get('/api/comments')
-        .then((response) => app.comments = response.data || [])
+    update: function () {
+      axios
+        .get("/api/comments")
+        .then((response) => (this.comments = response.data || []))
         .catch((error) => console.log(error));
-    }
-  }
-})
+    },
+  },
+  created() {
+    this.update();
+  },
+}).mount("#app");
